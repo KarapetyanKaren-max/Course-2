d56a0769b7001bd0ddcbc9756754bc360640c347
Karen <karapetyankaren33933@gmail.com>
2025-01-17 23:41:06 +0400

    Initial commit

---
+++ src/org/skypro/Product.java
@@ -0,0 +1,50 @@
+package org.skypro;
+
+import java.util.Objects;
+
+public class Product {
+
+
+    private String name;
+    private int price;
+
+    public Product(int price, String name) {
+        this.price = price;
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+}
---
+++ src/org/skypro/ProductBasket.java
@@ -0,0 +1,74 @@
+package org.skypro;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class ProductBasket {
+        private Product[] products = new Product[5];
+
+        // Этот класс содержит в себе одну корзину с продуктами, которые купил один пользователь
+        // Вам нужно реализовать следующие публичные методы
+        public void addProduct(Product product) {
+            for (int number = 0; number < products.length; number++) {
+                if (products[number] == null) {
+                    products[number] = product;
+                    System.out.println("Продукт в корзину добавлен");
+                    break;
+                }
+                if (number == products.length - 1) {
+                    System.out.println("корзина заполнена");
+                }
+            }
+        }
+        public int priceBasket() {
+            int sum = 0;
+            if (products[0] == null) {
+                System.out.println("Корзина пуста");
+            } else {
+                for (Product variable : products) {
+                    if (variable != null) {
+                        sum = sum+variable.getPrice();
+                    }
+                }
+                return sum;
+            }
+            return 0;
+        }
+        public void printBasket() {
+            if (products[0] == null) {
+                System.out.println("Корзина пуста");
+            }
+            for (Product variable : products) {
+                if (variable != null) {
+                    System.out.println(variable);
+                }
+            }
+            System.out.println("Цена корзины: " + priceBasket());
+        }
+        public void deleteProduct () {
+            for (int number = 0; number < products.length; number++) {
+                products[number] = null;
+            }
+            System.out.println("Корзина очищена");
+        }
+
+    @Override
+    public String toString() {
+        return "ProductBasket{" +
+                "products=" + Arrays.toString(products) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductBasket basket = (ProductBasket) o;
+        return Objects.deepEquals(products, basket.products);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(products);
+    }
+}
