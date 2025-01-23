4d42fd5c9adc681a5c235fb6891c1e4ab581bb51
Karen <karapetyankaren33933@gmail.com>
2025-01-23 19:50:20 +0400

    Initial commit

--- src/org/skypro/App.java
+++ src/org/skypro/App.java
@@ -4,11 +4,11 @@ package org.skypro;
         public static void main(String[] args) {
 
             ProductBasket basket = new ProductBasket();
-            basket.addProduct(new Product(150, "Молоко"));
-            basket.addProduct(new Product(450, "Рыба"));
-            basket.addProduct(new Product(25, "Хрен"));
-            basket.addProduct(new Product(120, "Масло"));
-            basket.addProduct(new Product(650, "Торт"));
+            basket.addProduct(new DiscountedProduct(50, "Молоко", 230));
+            basket.addProduct(new FixPriceProduct("Рыба"));
+            basket.addProduct(new SimpleProduct(25, "Хрен"));
+            basket.addProduct(new SimpleProduct(120, "Масло"));
+            basket.addProduct(new SimpleProduct(650, "Торт"));
             System.out.println();
             System.out.println("Добавить продукт в корзину");
             basket.printBasket();
--- src/org/skypro/Product.java
+++ src/org/skypro/Product.java
@@ -2,49 +2,43 @@ package org.skypro;
 
 import java.util.Objects;
 
-public class Product {
+public abstract class Product {
+
+    protected String name;
 
 
-    private String name;
-    private int price;
+    public Product(String name) {
 
-    public Product(int price, String name) {
-        this.price = price;
         this.name = name;
     }
     public String getName() {
         return name;
     }
+
+
     public void setName(String name) {
         this.name = name;
     }
 
-    public int getPrice() {
-        return price;
-    }
+    public abstract double getPrice();
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
+        @Override
+        public boolean equals (Object o){
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Product product = (Product) o;
+            return Objects.equals(name, product.name);
+        }
+
+        @Override
+        public int hashCode () {
+            return Objects.hashCode(name);
+        }
 
     @Override
     public String toString() {
         return "Product{" +
                 "name='" + name + '\'' +
-                ", price=" + price +
                 '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return price == product.price && Objects.equals(name, product.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, price);
     }
 }
